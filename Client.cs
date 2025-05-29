c15e1568c88fe6d40a1a11bbf1a7cbddc2486106
belyyfh <belyjegor45@gmail.com>
2025-05-29 09:58:25 +0300

    Create Client.cs

---
+++ Client.cs
@@ -0,0 +1,9 @@
+namespace ComputerClubApp.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
